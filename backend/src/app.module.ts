056e0130b84b7bac46d62d429eadc49a74ed735d
RochaEverton <rochaevertondev@gmail.com>
2025-11-14 21:22:16 -0300

    Estrutura inicial do backend com Banco de dados Local

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Nirv@n@1',
+      database: 'ibridge_db',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
